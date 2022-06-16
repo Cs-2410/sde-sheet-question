7a8123f9c83844fad5a2de1f3170f1fc4fbd1709
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-06-16 18:56:27 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (75.55%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-           int solve(vector<int> &nums){
-      int n = nums.size();
+    int rob(vector<int>& nums) {
+        int n = nums.size();
      int perv1 = nums[0];
      int perv2 = 0;
    for(int i = 1; i < n; i++){
@@ -12,9 +12,5 @@ public:
        perv1 = ans;
    }
     return perv1;
-}                  
-        
-         int rob(vector<int>& nums) {
-            return solve(nums);
     }
 };
