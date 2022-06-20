4141c430fcaa06ba91fd244940d5e0396c9a0076
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-06-20 12:36:24 +0530

    Time: 41 ms (34.60%), Space: 19.3 MB (57.40%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+            if(n == 1)
+                    return ;
+            int left = 0, right = 0;
+            while(right < n){
+                    if(nums[right] == 0)
+                            right++;
+                    else{
+                            swap(nums[left] , nums[right]);
+                            right++;
+                            left++;
+                    }
+            }
+    }
+};
