e7b787be9e94c81d2f9949ab907f885083df7f60
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-06-27 02:52:00 +0530

    Time: 71 ms (16.69%), Space: 6.5 MB (51.37%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+       bool ok(int i, int j, vector<vector<char>>& board, char val){
+               for(int x = 0; x < 9; x++){
+                       if(board[i][x] == val)
+                               return false;
+                       if(board[x][j] == val)
+                               return false;
+                       if(board[3*(i/3) + (x/3)][3*(j/3) + (x%3)] == val)
+                               return false;
+               }
+               return true;
+       }
+        
+        bool solve(vector<vector<char>>& board){
+                for(int i = 0; i < board.size(); i++){
+                       for(int j = 0; j < board[0].size(); j++){
+                               if(board[i][j] == '.'){
+                             for(char val = '1'; val <= '9'; val++){
+                                           if(ok(i , j , board , val)){
+                                                 board[i][j] = val;
+                                                 bool solpos = solve(board);
+                                                 if(solpos)
+                                                         return true;
+                                                   else
+                                                         board[i][j] = '.';
+                                           }    
+                                       }
+                                       return false;
+                               }
+                       }
+                }
+                return true;
+        }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
