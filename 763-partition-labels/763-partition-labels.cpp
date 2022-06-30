68eaf879ec44d178fa8740cdb515294182f1517c
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-06-30 22:24:23 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (25.97%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -3,35 +3,23 @@ public:
     vector<int> partitionLabels(string s) {
         
             int n=s.size();
-            vector<int> ans;
             map<char,int> mp;
-            for(auto ele:s)
-                    mp[ele]++;
-            int index=0,lastIndex=0,first=1;
-            map<char,int> tm;
+            for(int i=n-1;i>=0;i--){
+                    if(mp.find(s[i])==mp.end())
+                            mp[s[i]]=i;
+            }
+            int index=0,curmax=0,lastIndex=0,first=1;
+            vector<int> ans;
             while(index<n){
-                    tm[s[index]]++;
-                    int flag=0;
-                    for(int i=0;i<26;i++){
-                            char ch=(char)('a'+i);
-                            if(tm.find(ch)!=tm.end() && tm[ch]!=mp[ch]){
-                                    flag=1;
-                                    break;
-                            }
-                    }
-                    if(flag==0){
+                    curmax=max(curmax,mp[s[index]]);
+                    if(curmax==index){
                             if(first){
-                                ans.push_back(index+1);
-                                first=0;
+                                    ans.push_back(index+1);
+                                    first=0;
                             }
                             else
-                                ans.push_back(index-lastIndex);
+                                    ans.push_back(index-lastIndex);
                             lastIndex=index;
-                            for(int i=0;i<26;i++){
-                                    char ch=(char)('a'+i);
-                                    mp[ch]-=tm[ch];
-                            }
-                            tm.clear();
                     }
                     index++;
             }
