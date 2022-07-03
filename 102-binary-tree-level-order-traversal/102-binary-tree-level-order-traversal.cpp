97c181eb5de8a54c24a5572334292a21a1a18031
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-07-03 09:40:45 +0530

    Time: 3 ms (87.46%), Space: 12.6 MB (61.57%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -9,61 +9,57 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     vector<vector<int>> levelOrder(TreeNode* root) {
-//           queue<TreeNode* > q;
-//             vector<vector<int>> result; 
-//             vector<int> level;
-//             if(root == NULL){
-//                     q.push(root);
-//                     level.push_back(root->val);
-//                     result.push_back(level);
-//                     level.clear();
-//             }
-//             while(!q.empty()){
-//                     int i, n = q.size();
-//                     for(i = 0; i < n; i++){
-//                             TreeNode* p = q.front();
-//                             q.pop();
-//                             if(p->left != NULL){
-//                                     q.push(p->left);
-//                                     level.push_back(p->left->val);
-//                             }
-//                             if(p->right != NULL){
-//                                     q.push(p->right);
-//                                     level.push_back(p->right->val);
-//                             }
-//             }
-//                     if(!level.empty() && i == n){
-//                             result.push_back(level);
-//                             level.clear();
-//                     }
-//             }
-//           return result;        
-//     }
-// };
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-          queue<pair<TreeNode*,int> > q;
+          queue<TreeNode* > q;
             vector<vector<int>> result; 
             vector<int> level;
-            q.push({root,0});
-            int index=0;
+            if(root == NULL)
+                    return result;
+            q.push(root);
+            
             while(!q.empty()){
-                    auto ele=q.front();
-                    q.pop();
-                    if(ele.second>index){
-                            index=ele.second;
+                    int i, n = q.size();
+                    for(i = 0; i < n; i++){
+                            TreeNode* p = q.front();
+                            q.pop();
+                            level.push_back(p->val);
+                            if(p->left != NULL){
+                                    q.push(p->left);
+                            }
+                            if(p->right != NULL){
+                                    q.push(p->right);
+                            }
+                    }
                             result.push_back(level);
                             level.clear();
-                    }
-                    if(ele.first==NULL) continue;
-                    level.push_back(ele.first->val);
-                    q.push({ele.first->left,ele.second+1});
-                    q.push({ele.first->right,ele.second+1});
+                  
             }
           return result;        
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//           queue<pair<TreeNode*,int> > q;
+//             vector<vector<int>> result; 
+//             vector<int> level;
+//             q.push({root,0});
+//             int index=0;
+//             while(!q.empty()){
+//                     auto ele=q.front();
+//                     q.pop();
+//                     if(ele.second>index){
+//                             index=ele.second;
+//                             result.push_back(level);
+//                             level.clear();
+//                     }
+//                     if(ele.first==NULL) continue;
+//                     level.push_back(ele.first->val);
+//                     q.push({ele.first->left,ele.second+1});
+//                     q.push({ele.first->right,ele.second+1});
+//             }
+//           return result;        
+//     }
+// };
