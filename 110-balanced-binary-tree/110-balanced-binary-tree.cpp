dee61c99d6546082a5ea326ef51ca79492ac316b
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-07-04 08:30:24 +0530

    Time: 21 ms (47.99%), Space: 21.1 MB (14.98%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,23 +11,16 @@
  */
 class Solution {
 public:
-        int solve(TreeNode* root , int& ans){
-                if(root == NULL)
-                        return 0;
-                int leftsubtree = solve(root->left , ans);
-                int rightsubtree = solve(root->right , ans);
-                ans = max(ans,abs(leftsubtree-rightsubtree));
-                return (max(leftsubtree , rightsubtree) +1);
-                
+        bool ans=true;
+        int func(TreeNode* root){
+                if(!root) return 0;
+                int left=func(root->left),
+                    right=func(root->right);
+                if(abs(left-right)>1) ans=false;
+                return max(left,right)+1;
         }
-        bool isBalanced(TreeNode* root) {
-        if(root == NULL)
-                return true;
-       int ans = 0;
-                solve(root , ans);
-        if(ans > 1)
-                return false;
-                else 
-                return true;
+    bool isBalanced(TreeNode* root) {
+            func(root);
+            return ans;
     }
 };
