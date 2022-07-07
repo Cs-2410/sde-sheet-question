a456a5d002411aee7469bce10a30fdec340bd47c
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-07-07 21:50:17 +0530

    Time: 1521 ms (6.51%), Space: 7.1 MB (87.51%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,36 @@
+class Foo {
+public:
+    int *counter;
+    Foo() {
+        this->counter=new int(1);
+    }
+
+    void first(function<void()> printFirst) {
+        // printFirst() outputs "first". Do not change or remove this line.
+        
+            printFirst();
+        *counter=*counter+1;
+    }
+
+    void second(function<void()> printSecond) {
+        
+            // while(*counter<2){
+            //         cout<<*counter<<"--\n";
+            // }
+            while(*counter<2);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        *counter=*counter+1;
+    }
+
+    void third(function<void()> printThird) {
+        
+            // while(*counter<3){
+            //         cout<<*counter<<"++\n";
+            // }
+            while(*counter<3);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+        *counter=*counter+1;
+    }
+};
