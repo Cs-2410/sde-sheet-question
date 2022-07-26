6fcfbb9b35016cd2be42d0cabfe456468dca938b
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-07-26 08:59:11 +0530

    Time: 33 ms (89.03%), Space: 18.4 MB (67.18%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int flag  = 0;
+            int row = 0, col = -1;
+            int m = mat.size();
+            int n = mat[0].size();
+            vector<int> ans;
+            while(row<m || col<n){
+            if(flag==0){
+                    ++col;
+                    if(row==m){
+                            --row;
+                            ++col;
+                    }
+            while(row>=0 && col<n){
+                 ans.push_back(mat[row--][col++]);   
+            }
+                    flag = 1;
+            }
+            else if(flag){
+                    ++row;
+                    if(col==n){
+                            --col;
+                            ++row;
+                    }
+            while(col>=0 && row<m){
+                 ans.push_back(mat[row++][col--]);   
+            }
+                    flag = 0;
+            }
+            }
+            return ans;
+    }
+};
