653c914de0ffef3822a7f39bf6d878fc8f73364d
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-08-29 19:30:10 +0530

    Time: 88 ms (13.09%), Space: 18.1 MB (19.44%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-        
-        void dfs(vector<vector<char>>& image,int i, int j){
-                int n = image.size();
-                int m = image[0].size();
-                if(i<0 || j<0) return;
-                if(i>=n || j>=m) return;
-                if(image[i][j] != '1') return;
-                
-                image[i][j] = '0';
-                
-                dfs(image,i-1,j);
-                dfs(image,i+1,j);
-                dfs(image,i,j+1);
-                dfs(image,i,j-1);
-        }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    dfs(grid, i, j);
-                }
+        int m = grid.size(), n = m ? grid[0].size() : 0, island = 0,offsets[] = {0, 1, 0, -1, 0};
+            
+            for(int i = 0; i < m; i++){
+                    for(int j =0; j < n; j++){
+                            if(grid[i][j]=='1'){
+                                    island++;
+                                    queue<pair<int ,int>> q;
+                                    q.push({i,j});
+                                    grid[i][j]='0';
+                                    while(!q.empty()){
+                                            int row = q.front().first;
+                                            int col = q.front().second;
+                                            q.pop();
+                                           for (int k = 0; k < 4; k++) {
+                            int r = row + offsets[k], c = col + offsets[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
+                                grid[r][c] = '0';
+                                q.push({r, c});
+                            }
+                        }
+                                            
+                                    }
+                            }
+                    }
             }
-        }
-        return islands;
+            return island;
     }
 };
