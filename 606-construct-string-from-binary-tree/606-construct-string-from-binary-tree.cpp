d931f1ad5f6e77b602c4cad89c91f11b2824fb07
Chetan Sinha <99377304+Cs-2410@users.noreply.github.com>
2022-09-07 19:04:56 +0530

    Time: 7 ms (99.94%), Space: 23.4 MB (87.31%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -11,23 +11,33 @@
  */
 class Solution {
 public:
-    string tree2str(TreeNode* root) {
-        string s = to_string(root->val);
-            
-            if(root->left){
-                    s += "(";
-                    s += tree2str(root->left);
+        void solve(TreeNode* root, string& s){
+                if(root==NULL) return;
+                
+                s += to_string(root->val);
+                
+                if(root->left){
+                        s += "(";
+                     solve(root->left,s);
                     s += ")";
-            }
-            
-           if(root->right){
-                   if(!root->left) {
-                           s += "()";
-                   }
-                   s += "(";
-                   s +=  tree2str(root->right);
+                }
+                
+                if(root->right){
+                        if(!root->left){
+                                s += "()";
+                        }
+                        s += "(";
+                     solve(root->right,s);
                    s += ")";
-           }
+                }
+                return;
+        }
+    string tree2str(TreeNode* root) {
+        if(!root) return "";
+            
+            string s = "";
+            
+            solve(root,s);
             
             return s;
     }
